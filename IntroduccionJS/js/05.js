f21874c8db7296c695e58e6932a5b3feb3da0e75
Luis91033 <luiscorralcorona@gmail.com>
2022-01-07 15:41:56 -0600

    Se agregarón más archivos para aprender de js

---
+++ IntroduccionJS/js/05.js
@@ -0,0 +1,7 @@
+//Números
+
+const numero1 = 100;
+const numero2 = 200;
+const numero3 = 200.20;
+
+console.log(numero1);
---
+++ IntroduccionJS/js/06.js
@@ -0,0 +1,10 @@
+//Objeto Math
+
+let resultado;
+
+resultado = Math.PI;
+resultado = Math.round(2.5);
+resultado = Math.ceil(2.1);
+
+
+console.log(resultado);

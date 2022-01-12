d0007df841f799dd9396e481221f1412edfb51c9
Luis91033 <luiscorralcorona@gmail.com>
2022-01-11 20:13:25 -0600

    Se agregarón más archivos para aprender de js

---
+++ IntroduccionJS/js/07.js
@@ -0,0 +1,7 @@
+//Orden de las operaciones
+
+let resultado;
+
+resultado = (20 + 30) *2;
+
+console.log(resultado);
---
+++ IntroduccionJS/js/08.js
@@ -0,0 +1,9 @@
+//Concatenar
+
+const nombre = 'Luis';
+const email = 'luis@correo.com';
+
+console.log(nombre + email);
+
+//Template strings
+console.log(`Nombre Cliente: ${nombre} Email: ${email}`)

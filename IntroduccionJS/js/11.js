bb70c55e07b1b1bf02b2e280b46812e710468547
Luis91033 <luiscorralcorona@gmail.com>
2022-05-29 20:04:48 -0500

    Se modificaron algunos archivos de js

--- IntroduccionJS/js/11.js
+++ IntroduccionJS/js/11.js
@@ -1,16 +1,17 @@
 //Objetos
 const producto = {
-    nombreProducto: "Monitor 20 Pulgadas",
-    precio: 300, 
-    disponible: true 
-}
+  nombreProducto: "Monitor 20 Pulgadas",
+  precio: 300,
+  disponible: true,
+};
 
 //Forma anterior de hacerlo
 const precioproducto = producto.precio;
 
-console.log(precioproducto);
+//console.log(precioproducto);
 
 //Destructuring
-const {precio} = producto;
+const { precio } = producto;
+console.log(precio);
 
 //Extraes el valor de un arreglo
--- IntroduccionJS/js/15.js
+++ IntroduccionJS/js/15.js
@@ -1,36 +1,36 @@
-const meses = ['enero', 'febrero', 'marzo', 'abril', 'mayo'];
+const meses = ["enero", "febrero", "marzo", "abril", "mayo"];
 
 const carrito = [
-    {nombre: 'Monitor 20 pulgadas', precio: 500},
-    {nombre: 'Televisión 50 pulgadas', precio: 700},
-    {nombre: 'Tablet', precio: 300},
-    {nombre: 'Audífonos', precio: 200},
-    {nombre: 'Teclado', precio: 50},
+  { nombre: "Monitor 20 pulgadas", precio: 500 },
+  { nombre: "Televisión 50 pulgadas", precio: 700 },
+  { nombre: "Tablet", precio: 300 },
+  { nombre: "Audífonos", precio: 200 },
+  { nombre: "Teclado", precio: 50 },
 ];
 
 //forEach
-meses.forEach(function(mes){
-    if(mes == 'marzo'){
-        console.log('Marzo si existe');
-    }
+meses.forEach(function (mes) {
+  if (mes == "marzo") {
+    console.log("Marzo si existe");
+  }
 });
 
 //Includes
-let resultado = meses.includes('marzo');
+let resultado = meses.includes("marzo");
 
 //Some ideal para arreglo de objetos
-resultado = carrito.some(function(producto) {
-    return producto.nombre === 'Tablet'
-}) 
+resultado = carrito.some(function (producto) {
+  return producto.nombre === "Tablet";
+});
 
 //Reduce
-resultado = carrito.reduce(function(total, producto) {
-    return total + producto.precio
-}, 0)
+resultado = carrito.reduce(function (total, producto) {
+  return total + producto.precio;
+}, 0);
 
 //Filter
-resultado = carrito.filter(function(producto) {
-    return producto.precio > 400
+resultado = carrito.filter(function (producto) {
+  return producto.precio > 400;
 });
 
 console.log(resultado);

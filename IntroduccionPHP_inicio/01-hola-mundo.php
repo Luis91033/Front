09d72de2e77d5d78ba97fe8b6c34003be7ff4cd0
Luis91033 <luiscorralcorona@gmail.com>
2022-07-06 17:24:39 -0500

    [ADDED] Archivos para trabajar con PHP

---
+++ IntroduccionPHP_inicio/01-hola-mundo.php
@@ -0,0 +1,13 @@
+<?php include "includes/header.php ";
+
+echo "hola Mundo";
+
+echo("Hola Mundo");
+
+print ("Hola mundo");
+
+print_r("Hola mundo");
+
+var_dump("Hola mundo"); 
+
+include "includes/footer.php";
---
+++ IntroduccionPHP_inicio/02-variables.php
@@ -0,0 +1,14 @@
+<?php include 'includes/header.php';
+
+$nombre = "Eduardo";
+$_nombre = "Xavier";
+
+
+echo $nombre;
+var_dump($nombre);
+
+define('constante', 'es el valor de la constante'); //Se definen constantes
+
+echo constante;
+
+include 'includes/footer.php';
